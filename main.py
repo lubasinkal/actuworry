f981247518a5eb06f469ab6ece76c0748a0c4197
lubasinkal <lubasinkal@outlook.com>
2025-07-31 00:06:58 +0200

    Start

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 router = APIRouter()
 
-@router.get("/")
+@router.post("/")
 async def hello():
     return {"hello": "hello"}
 
 app.include_router(router)
-
-
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
